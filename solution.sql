df2ec5f1b1971ca6f713d3f3f2488e8e053fe8ec
EricaMarzi <erica.marzi.1993@gmail.com>
2024-03-04 18:15:22 +0100

    group by

--- solution.sql
+++ solution.sql
@@ -35,6 +35,18 @@ WHERE `phone` IS NULL;
 
 -- GROUP BY
 -- 1. Contare quanti iscritti ci sono stati ogni anno
+SELECT YEAR(`enrolment_date`) AS `year_of_registration`, COUNT(*) AS `n_registered`
+FROM `students`
+GROUP BY `year_of_registration`;
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+SELECT `office_address`,  COUNT(*) AS `n_teachers` 
+FROM `teachers`
+GROUP BY `office_address`;
 -- 3. Calcolare la media dei voti di ogni appello d'esame
+SELECT ROUND(AVG(`vote`)) AS `point_average`, `exam_id`
+FROM `exam_student`
+GROUP BY `exam_id`;
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+SELECT COUNT(*) AS `n_courses`, `department_id`
+FROM `degrees`
+GROUP BY `department_id`;
